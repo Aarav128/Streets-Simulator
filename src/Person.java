b4df80c144e6ce86640be4e3acfa2458ac0d0040
Luke8934 <ls7290@pleasantonusd.net>
2025-03-27 14:42:11 -0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,10 @@
+import java.utll.ArrayList;
 public class Person {
-
-    
+Private String name;
+Private int age;
+Private boolean isHealthy;//if true no problem if false chance of dying
+Private int salary;
+Private String job;
+Private int popularity;// ranges 1-100
+Private ArrayList<items>;
 }
