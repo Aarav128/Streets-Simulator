38c074f89fdbe6aec72b5b94249fb3588e102a72
Aarav Shah <codingaarav1@gmail.com>
2025-03-27 14:45:13 -0700

    created outline for Location.java

--- src/Location.java
+++ src/Location.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Location {
-    
+    private ArrayList<Location> adjacentLocations;
+    private ArrayList<Person> characters;
+    private ArrayList<Item> items; // these are items not held by any player. 
+    private String name;
+
+    public Location(String name) {
+        this.name = name;
+    }
+    public void createAdjacency(Location other) {
+        adjacentLocations.add(other);
+        if(!other.getAdjacentLocations().contains(this)) {
+            other.createAdjacency(this);
+        }
+    }
+
+    public ArrayList<Location> getAdjacentLocations() {
+        return adjacentLocations;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
