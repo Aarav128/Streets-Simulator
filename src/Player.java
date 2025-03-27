03b97e9cc502f4cdcb4829d9c276e12a2af96acb
Luke8934 <ls7290@pleasantonusd.net>
2025-03-27 22:08:35 +0000

    jkjk

--- src/Player.java
+++ src/Player.java
@@ -10,7 +10,7 @@ public class Player extends Person{
     public Player(String mom,String dad,String home,String itemInHand,int  money,boolean hasCar, boolean hasBike, String school)
     this.mom = mom;
     this.dad = dad;
-    this.home = hom;
+    this.home = home;
     this.itemInHand = itemInHand;
     this.money = money;
     this.hasCar = hasCar;
